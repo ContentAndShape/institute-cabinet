c55c4afe99eb300597ca3b4bb11f94fc76ea4849
Alexander <alexandr.s.trushnikov@gmail.com>
2022-10-16 21:12:34 +0500

    Enhanced models

--- src/cabinet/usr_cabinet/models.py
+++ src/cabinet/usr_cabinet/models.py
@@ -4,12 +4,18 @@ from django.contrib.auth.models import User
 
 class UserReprMixin(User):
 
+    class Meta:
+        ordering = ['last_name']
+
     def __str__(self) -> str:
         return self.get_full_name()
 
 
 class NameMixin(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']
 
     def __str__(self) -> str:
         return self.name
@@ -20,11 +26,11 @@ class Course(NameMixin):
 
 
 class Institute(NameMixin):
-    ...
+    scientific_field = models.CharField(max_length=50)
 
 
 class Student(UserReprMixin):
-    institute = models.ForeignKey('Institute', on_delete=models.CASCADE)
+    institute = models.ForeignKey('Institute', null=True, on_delete=models.SET_NULL)
 
 
 class Teacher(UserReprMixin):
