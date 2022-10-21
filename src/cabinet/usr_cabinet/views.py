685902d8cdbff055e6cf67c627df8cea5f8d4275
Alexander <alexandr.s.trushnikov@gmail.com>
2022-10-21 19:00:52 +0500

    Added base cabinet pages for users

--- src/cabinet/usr_cabinet/views.py
+++ src/cabinet/usr_cabinet/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def cabinet(request: HttpRequest) -> HttpResponse:
+
+    if request.user.is_authenticated == False:
+        return redirect("login/")
+
+    context = {
+        'user': request.user,
+        'user_type': request.user.__class__.__name__,
+    }
+    return render(request, 'usr_cabinet/base_cabinet.html', context)
