2e1ef7a160cff6a75b6748aa04e3408a827bfc83
Alexander <alexandr.s.trushnikov@gmail.com>
2022-10-21 14:59:36 +0500

    Registered models in admin panels

--- src/cabinet/usr_cabinet/admin.py
+++ src/cabinet/usr_cabinet/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Course, Institute, Student, Teacher
 
-# Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Institute)
+admin.site.register(Student)
+admin.site.register(Teacher)
