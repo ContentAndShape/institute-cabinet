b7e3d45fd276c858cccc0b712e4db68511aba49c
Alexander <alexandr.s.trushnikov@gmail.com>
2022-11-22 22:33:16 +0500

    Added tests users registration and models relations

--- src/cabinet/usr_cabinet/tests.py
+++ src/cabinet/usr_cabinet/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
-from .models import User
+from .models import User, UserType, Student, Teacher
+from cabinet.settings import ALLOWED_USER_TYPES
 
 
 def get_user(type: str) -> dict:
@@ -8,38 +9,95 @@ def get_user(type: str) -> dict:
         'email': f'{type}@test.com',
         'type': type,
         'first_name': 'Test',
-        'last_name': type.capitalize(),
+        'last_name': ALLOWED_USER_TYPES[type].capitalize(),
         'password': '123',
     }
 
 
-ADMIN = get_user('admin')
-STUDENT = get_user('student')
-TEACHER = get_user('teacher')
+STAFF = get_user(1)
+STUDENT = get_user(2)
+TEACHER = get_user(3)
 
 
-class UserTestCase(TestCase):
+class UserCreationTestCase(TestCase):
     def setUp(self) -> None:
-        User.objects.create_user(**ADMIN)
+        User.objects.create_user(**STAFF)
         User.objects.create_user(**STUDENT)
         User.objects.create_user(**TEACHER)
 
-    def test_user_fields(self):
-        admin = User.objects.get(email=ADMIN['email'])
+    def test_user_creation(self):
+        staff = User.objects.get(email=STAFF['email'])
         student = User.objects.get(email=STUDENT['email'])
         teacher = User.objects.get(email=TEACHER['email'])
 
-        for key, val in ADMIN.items():
-            self.assertEqual(val, admin.__dict__[key])
+        self.assertEqual(staff, User.objects.get(email=STAFF['email']))
+        self.assertEqual(student, User.objects.get(email=STUDENT['email']))
+        self.assertEqual(teacher, User.objects.get(email=TEACHER['email']))
+        
 
-        for key, val in STUDENT.items():
-            self.assertEqual(val, student.__dict__[key])
+    def test_user_fields(self):
+        staff = User.objects.get(email=STAFF['email'])
+        student = User.objects.get(email=STUDENT['email'])
+        teacher = User.objects.get(email=TEACHER['email'])
 
-        for key, val in TEACHER.items():
-            self.assertEqual(val, teacher.__dict__[key])
+        #TODO test user fields values
 
-    def test_user_cascade_delete(self):
-        '''
-        Test cascade deletion of admin, student, teacher after user instance delteion
-        '''
+    def test_invalid_fields(self):
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+
+        # No password
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email='test@t.com', type=1)
+
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email='test2@t.com', type='invalid')
+
+
+class UserRelationsTestCase(TestCase):
+    def setUp(self) -> None:
+        User.objects.create_user(**STAFF)
+        User.objects.create_user(**STUDENT)
+        User.objects.create_user(**TEACHER)
+
+    def test_user_type_table(self):
+        staff = User.objects.get(email=STAFF['email'])
+        student = User.objects.get(email=STUDENT['email'])
+        teacher = User.objects.get(email=TEACHER['email'])
+
+        staff_type = UserType.objects.get(type='staff')
+        self.assertEqual(staff_type, staff.type)
+
+        student_type = UserType.objects.get(type='student')
+        self.assertEqual(student_type, student.type)
+
+        teacher_type = UserType.objects.get(type='teacher')
+        self.assertEqual(teacher_type, teacher.type)
+
+        # TODO test type set to null after type del in type table
+
+    def test_student_table(self):
+        student_user = User.objects.get(email=STUDENT['email'])
+        Student.objects.get(user_id=student_user.id)
+
+        with self.assertRaises(Student.DoesNotExist):
+            Student.objects.get(user_id=10000)
+
+        # TODO test cascade drop after user del
+
+    def test_teacher_table(self):
+        teacher_user = User.objects.get(email=TEACHER['email'])
+        Teacher.objects.get(user_id=teacher_user.id)
+
+        with self.assertRaises(Teacher.DoesNotExist):
+            Teacher.objects.get(user_id=10000)
+
+        # TODO test cascade drop after user del
+
+    def test_institute_table(self):
         ...
+        # TODO test institute relation to students
+
+    def test_course_table(self):
+        ...
+        # TODO test course relation to teachers
