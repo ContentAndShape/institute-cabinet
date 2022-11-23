ac7796ec286a9ca453a0da4bbf8e1a88662f2850
Alexander <alexandr.s.trushnikov@gmail.com>
2022-11-23 22:05:58 +0500

    Field rename

--- src/cabinet/usr_cabinet/models.py
+++ src/cabinet/usr_cabinet/models.py
@@ -113,7 +113,7 @@ class User(AbstractBaseUser):
 
 
 class Course(NameMixin):
-    related_institute = models.ManyToManyField(
+    institutes = models.ManyToManyField(
         "Institute",
         default=None,
     )
